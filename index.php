22f1930c232baff759526622b7a9356d4e347cd9
Nathan <nathan.agu.2103@gmail.com>
2024-02-10 19:56:14 +0100

    Add controller

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
-    
+    include 'controller.php';
+
+    if (!isset($controller))
+    {
+        // Instanciation du controleur
+        $controller = new controller();
+    }
+
+    // Appel de la méthode d'affichage de l'entête
+    $controller->displayHeader();
+
+    // Appel de la méthode d'affichage de la page
+    $controller->displayPage();
+
+    // Appel de la méthode d'affichage du pied de page
+    $controller->displayFooter();
 ?>
