1849e836cdc9710469190c698b566bafd546efe4
Nathan <nathan.agu.2103@gmail.com>
2024-02-10 20:06:56 +0100

    Class controller

--- controller.php
+++ controller.php
@@ -1,3 +1,27 @@
 <?php
+    class controller
+    {
+        // Constructeur de la classe "controleur"
+        public function __construct(){}
 
+        // ========================= Parties à afficher =========================
+        
+        // Méthode pour afficher l'entête de la page du site
+        public function displayHeader()
+        {
+            require 'header.php';
+        }
+
+        // Méthode pour afficher la page du site (Contenu central)
+        public function displayPage()
+        {
+            require 'page.php';
+        }
+
+        // Méthode pour afficher le pied de la page du site
+        public function displayFooter()
+        {
+            require 'footer.php';
+        }
+    }
 ?>
