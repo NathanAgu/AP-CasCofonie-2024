2a87af9ae210d8cfdd9895ccd2faa9f4dd05e44b
Nathan <nathan.agu.2103@gmail.com>
2024-02-11 10:30:54 +0100

    Setter institution

--- institution.php
+++ institution.php
@@ -18,5 +18,20 @@
                     break;
             }
         }
+
+        public function __set($attribute, $value)
+        {
+            switch ($attribute)
+            {
+                case "id":
+                    $this->id = $value;
+                    break;
+                case "libelle":
+                    $this->libelle = $value;
+                    break;
+                case "typeInstitution":
+                    $this->typeInstitution = $value;
+            }
+        }
     }
 ?>
