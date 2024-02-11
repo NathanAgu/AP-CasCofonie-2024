ffb14ff5764226b6966f7e2dcdf7bb7494183108
Nathan <nathan.agu.2103@gmail.com>
2024-02-11 10:17:44 +0100

    Getter typeInstitution

--- typeInstitution.php
+++ typeInstitution.php
@@ -2,5 +2,18 @@
     class typeInstitution
     {
         public function __construct(private int $id, private string $libelle){}
+
+        public function __get($attribute)
+        {
+            switch ($attribute)
+            {
+                case "id":
+                    return $this->id;
+                    break;
+                case "libelle":
+                    return $this->libelle;
+                    break;
+            }
+        }
     }
 ?>
