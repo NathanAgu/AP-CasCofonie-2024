71713ef694f737d74fca277424dea73e05f1a1f2
Nathan <nathan.agu.2103@gmail.com>
2024-02-12 15:04:11 +0100

    Class & Construct+addInstitution

---
+++ containerInstitution.php
@@ -0,0 +1,23 @@
+<?php
+    include_once('institution.php');
+
+    class containerInstitution
+    {
+        private $institutions;
+
+        public function __construct()
+        {
+            $this->$institutions = new ArrayObject();
+        }
+
+        public function addInstitution($id, $libelle, $typeInstitution)
+        {
+            $this->$institutions->append(new institution($id, $libelle, $typeInstitution));
+        }
+
+        public function listInstitutions()
+        {
+            
+        }
+    }
+?>
