675e5e58817795d83a558ccf60ae2f34fe2a033b
Nathan <nathan.agu.2103@gmail.com>
2024-02-14 10:22:23 +0100

    Autoloader function

--- tools/autoloader.php
+++ tools/autoloader.php
@@ -1,3 +1,39 @@
 <?php
+    function autoloader($class)
+    {
+        $result = substr($class, 0, 5);
 
+        if (strcmp($result, 'contr') == 0)
+        {
+            include_once 'Controller/'.$class.'.php';
+        }
+        else
+        {
+            if (strcmp($result, 'acces') == 0)
+            {
+                include_once 'Tools/'.$class.'.php';
+            }
+            else
+            {
+                if (strcmp($result, 'conta') == 0)
+                {
+                    include_once 'Processing/Containers/'.$class.'.php';
+                }
+                else
+                {
+                    if (strcmp($result, 'job') == 0)
+                    {
+                        include_once 'Processing/Jobs/'.$class.'.php';
+                    }
+                    else
+                    {
+                        if (strcmp($result, 'view') == 0)
+                        {
+                            include_once 'Templates/Views/'.$class.'.php';
+                        }
+                    }
+                }
+            }
+        }
+    }
 ?>
