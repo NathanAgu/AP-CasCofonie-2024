b1ec1c899a279e21c9a97ac914b790ec0324918f
Nathan <nathan.agu.2103@gmail.com>
2024-02-14 14:50:33 +0100

    Modifs

---
+++ processing/containers/containerTypeInstitution.php
@@ -0,0 +1,26 @@
+<?php
+    autoloader("actionArticle");
+
+    class containerTypeInstitution
+    {
+        private $typeInstitutions;
+
+        public function __construct()
+        {
+            $this->$typeInstitutions = new ArrayObject();
+        }
+
+        public function addTypeInstitution($id, $libelle)
+        {
+            $this->$typeInstitutions->append(new typeInstitutions($id, $libelle));
+        }
+
+        public function listTypeInstitution()
+        {
+            foreach ($this->typeInstitutions as $typeInstitution)
+            {
+                
+            }
+        }
+    }
+?>
