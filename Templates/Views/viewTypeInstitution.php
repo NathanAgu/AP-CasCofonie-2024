6485c866b3d25a65752e6c15fa18a89f834b5cdd
Nathan <nathan.agu.2103@gmail.com>
2024-02-21 17:22:21 +0100

    View typeInstitution

---
+++ Templates/Views/viewTypeInstitution.php
@@ -0,0 +1,11 @@
+<?php
+    class viewTypeInstitution
+    {
+        public function __construct(){}
+
+        public function displayTypeInstitution()
+        {
+            echo "Page d'affichage typeInstitution.";
+        }
+    }
+?>
