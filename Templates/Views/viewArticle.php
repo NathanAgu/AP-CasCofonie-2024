b2a9ccce9caf5677a107770b63d114963d471119
Nathan <nathan.agu.2103@gmail.com>
2024-02-21 17:13:13 +0100

    View article

---
+++ Templates/Views/viewArticle.php
@@ -0,0 +1,21 @@
+<?php
+    class viewArticle
+    {
+        public function __construct(){}
+
+        public function displayArticle()
+        {
+            echo "Page d'affichage d'ammendement.";
+        }
+
+        public function addArticle()
+        {
+            echo "Page d'ajout d'ammendement.";
+        }
+
+        public function removeArticle()
+        {
+            echo "Page de suppression d'ammendement.";
+        }
+    }
+?>
