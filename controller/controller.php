fe9b84497e9cd2ca7d097d49b6ea815a6aac221c
Nathan <nathan.agu.2103@gmail.com>
2024-02-22 12:32:17 +0100

    Controller amendment

--- controller/controller.php
+++ controller/controller.php
@@ -15,13 +15,56 @@
         // Méthode pour afficher la page du site (Contenu central)
         public function displayPage()
         {
-            //require 'page.php';
+            if (isset($_GET['view']) && isset($_GET['action']))
+            {
+                $view = $_GET['view'];
+                $action = $_GET['action'];
+
+                switch ($view)
+                {
+                    case "amendment":
+                        $this->controllerAmendment($action);
+                        break;
+                    case "article":
+                        break;
+                    case "intitution":
+                        break;
+                    case "organ":
+                        break;
+                    case "role":
+                        break;
+                    case "text":
+                        break;
+                    case "typeInstitution":
+                        break;
+                }
+            }
         }
 
         // Méthode pour afficher le pied de la page du site
         public function displayFooter()
         {
             require 'Templates/Views/viewsFooter.php';
+        }
+
+        // Controlleurs pour chaque class
+        public function controllerAmendment($action)
+        {
+            switch ($action)
+            {
+                case "display":
+                    $view = new viewAmendment();
+                    $view->displayAmendment();
+                    break;
+                case "add":
+                    $view = new viewAmendment();
+                    $view->addAmendment();
+                    break;
+                case "remove":
+                    $view = new viewAmendment();
+                    $view->removeAmendment();
+                    break;
+            }
         }
     }
 ?>
