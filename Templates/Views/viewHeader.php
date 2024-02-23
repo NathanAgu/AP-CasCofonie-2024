7de4abf5a87b7934aad8af743426a557beb7eb15
Nathan <nathan.agu.2103@gmail.com>
2024-02-23 14:26:21 +0100

    Update #3

--- Templates/Views/viewHeader.php
+++ Templates/Views/viewHeader.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="Templates/Assets/Css/style.css">
+
     <title>Document</title>
 </head>
 <body>
