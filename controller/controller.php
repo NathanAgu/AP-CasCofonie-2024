5bf64c94c7b29ce23226e1a72d9653e6a1308f19
Balkhubal86 <baptiste86@hotmail.com>
2024-02-23 14:55:28 +0100

    "Changement de Controller "

--- controller/controller.php
+++ controller/controller.php
@@ -1,41 +1,168 @@
 <?php
-include_once ('processing/containers/containerText.php');
+    include_once("autoload.php");
+
+    autoload("viewAmendment");
+    autoload("viewArticle");
+    autoload("viewInstitution");
+    autoload("viewOrgan");
+    autoload("viewRole");
+    autoload("viewText");
+    autoload("viewTypeInstitution");
+
     class controller
     {
-        
-        /*********************************************************************************************************************
-                                    CONSTRUCTEUR DE NOTRE CONTROLEUR
-		        On construit tous les tableux d'objets et on les remplis vec la base de données
-        *********************************************************************************************************************/
+        // Constructeur de la classe "controleur" 
+        public function __construct(){}
 
-        public function __construct()
-        {
-            
-        }
+        // ========================= Parties à afficher =========================
 
-        /*****************************************************************************************
-                                    AFFICHAGE DES ENTETES ET PIED DE PAGE
-        ******************************************************************************************/
-
+        // Méthode pour afficher l'entête de la page du site
         public function displayHeader()
         {
-            require 'Templates/Views/viewsHeader.php';
+            require 'Templates/Views/viewHeader.php';
         }
 
+        // Méthode pour afficher la page du site (Contenu central)
+        public function displayPage()
+        {
+            if (isset($_GET['view']) && isset($_GET['action']))
+            {
+                $view = $_GET['view'];
+                $action = $_GET['action'];
+
+                switch ($view)
+                {
+                    case "amendment":
+                        $this->controllerAmendment($action);
+                        break;
+                    case "article":
+                        $this->controllerArticle($action);
+                        break;
+                    case "institution":
+                        $this->controllerInstitution($action);
+                        break;
+                    case "organ":
+                        $this->controllerOrgan($action);
+                        break;
+                    case "role":
+                        $this->controllerRole($action);
+                        break;
+                    case "text":
+                        $this->controllerText($action);
+                        break;
+                    case "typeInstitution":
+                        $this->controllerTypeInstitution($action);
+                        break;
+                }
+            }
+        }
+
+        // Méthode pour afficher le pied de la page du site
         public function displayFooter()
         {
             require 'Templates/Views/viewsFooter.php';
         }
 
-
-        /******************************************************************************************
-                        EN FONCTION DE LA VUE DEMANDE ON EFFECTUE TELLE OU TELLE ACTION
-        ********************************************************************************************/
-        
-        // Méthode pour afficher la page du site (Contenu central)
-        public function displayPage()
+        // ================================ Controleur classes ================================
+        public function controllerAmendment($action)
         {
-            //require 'page.php';
+            switch ($action)
+            {
+                case "display":
+                    $view = new viewAmendment();
+                    $view->displayAmendment();
+                    break;
+                case "add":
+                    $view = new viewAmendment();
+                    $view->addAmendment();
+                    break;
+                case "remove":
+                    $view = new viewAmendment();
+                    $view->removeAmendment();
+                    break;
+            }
+        }
+
+        public function controllerArticle($action)
+        {
+            switch ($action)
+            {
+                case "display":
+                    $view = new viewArticle();
+                    $view->displayArticle();
+                    break;
+                case "add":
+                    $view = new viewArticle();
+                    $view->addArticle();
+                    break;
+                case "remove":
+                    $view = new viewArticle();
+                    $view->removeArticle();
+                    break;
+            }
+        }
+
+        public function controllerInstitution($action)
+        {
+            switch ($action)
+            {
+                case "display":
+                    $view = new viewInstitution();
+                    $view->displayInstitution();
+                    break;
+            }
+        }
+
+        public function controllerOrgan($action)
+        {
+            switch ($action)
+            {
+                case "display":
+                    $view = new viewOrgan();
+                    $view->displayOrgan();
+                    break;
+            }
+        }
+
+        public function controllerRole($action)
+        {
+            switch ($action)
+            {
+                case "display":
+                    $view = new viewRole();
+                    $view->displayRole();
+                    break;
+            }
+        }
+
+        public function controllerText($action)
+        {
+            switch ($action)
+            {
+                case "display":
+                    $view = new viewText();
+                    $view->displayText();
+                    break;
+                case "add":
+                    $view = new viewText();
+                    $view->addText();
+                    break;
+                case "remove":
+                    $view = new viewText();
+                    $view->removeText();
+                    break;
+            }
+        }
+
+        public function controllerTypeInstitution($action)
+        {
+            switch ($action)
+            {
+                case "display":
+                    $view = new viewTypeInstitution();
+                    $view->displayTypeInstitution();
+                    break;
+            }
         }
     }
 ?>
