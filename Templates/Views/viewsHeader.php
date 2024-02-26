37529a1e5e2c52d792a47c6dbd26e5a666686065
Balkhubal86 <baptiste86@hotmail.com>
2024-02-26 15:00:43 +0100

    actualisation

--- Templates/Views/viewsHeader.php
+++ Templates/Views/viewsHeader.php
@@ -8,28 +8,30 @@
   <title>Header</title>
 </head>
 <body>
-
+    <!-- ENTETE -->
     <div class="p-3 bg-dark text-white">
       <div class="flexMain">
         <div class="flex1">
           
         </div>
-        <div class="flex2 text-center">
-          <div><strong>Le service public de la diffusion du droit</strong></div>
+        <div class="flex2 text-center" style="transform: translate(50%);">
+          <div><h5>Le service public de la diffusion du droit</h5></div>
         </div>
         <div class="flex3 text-end">
           <div><img src="Templates/Assets/Img/Drapeau.png" alt="Drapeau Cofonie" style="width: 8%;"></div>
         </div>
       </div>
     </div>
+    <!-- FIN ENTETE -->
 
+    <!-- ENTETE 2 AVEC MENU -->
     <div id="menuHolder">
       <div role="navigation" class="sticky-top border-bottom border-top" id="mainNavigation">
         <div class="flexMain">
           <div class="flex2">
             <button class="whiteLink siteLink" style="border-right:1px solid #eaeaea" onclick="menuToggle()"><i class="fas fa-bars me-2"></i> MENU</button>
           </div>
-          <div class="flex3 text-center" id="siteBrand">
+          <div class="flex3 text-center">
             <img src="Templates/Assets/Img/logo.png" alt="Logo" style="width: 25%;">
           </div>
     
@@ -37,12 +39,13 @@
             <button class="whiteLink siteLink"><i class="fas fa-search"></i></button>
           </div>
     
-          <div class="flex2 text-end d-none d-md-block">
+          <div class="flex2 text-end d-none d-md-block" style="transform: translate(-2.5%);">
             <button class="blackLink siteLink">Connexion</button>
           </div>
         </div>
       </div>
-    
+
+      <!-- MENU -->
       <div id="menuDrawer">
         <div class="p-4 border-bottom">
           <div class='row'>
@@ -61,6 +64,9 @@
           <a href="#" class="nav-menu-item"><i class="fas fa-building me-3"></i>Connexion</a>
         </div>
       </div>
+      <!-- FIN MENU -->
+
     </div>
+    <!-- FIN ENTETE 2 AVEC MENU -->
 
 <script src="Templates/Assets/Js/jsHeader.js"></script>
