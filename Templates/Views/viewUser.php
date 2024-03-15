db797fb717f4f93cb537e991eff628cf3c3c3084
Nathan <nathan.agu.2103@gmail.com>
2024-03-15 21:04:26 +0100

    file view user + class

---
+++ Templates/Views/viewUser.php
@@ -0,0 +1,26 @@
+<?php
+    class viewUser
+    {
+        public function __construct(){}
+
+        public function displayUserFormRegister()
+        {
+
+        }
+
+        public function displayUserFormLogin()
+        {
+
+        }
+
+        public function displayUserDashboard()
+        {
+
+        }
+
+        public function userLogout()
+        {
+
+        }
+    }
+?>
